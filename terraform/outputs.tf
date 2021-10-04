d4a0c0545216538ba74fbfab80355fbb629a7e3a
Tim <37308567+Timpng98@users.noreply.github.com>
2021-10-03 20:56:19 -0400

    Create outputs.tf

---
+++ terraform/outputs.tf
@@ -0,0 +1,9 @@
+output "juiceshop_ip" {
+  value = "ssh ec2-user@${module.juiceshop.juiceshop_ip}"
+}
+output "linux_ip" {
+  value = "ssh ec2-user@${module.linux.linux_ip}"
+}
+output "win_ip" {
+  value = module.win.win_ip
+}
