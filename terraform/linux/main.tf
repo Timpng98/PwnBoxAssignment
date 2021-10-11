d5d8729e16e0f96425d15a11e939be49163729b8
Tim <37308567+Timpng98@users.noreply.github.com>
2021-10-11 16:19:56 -0400

    Update main.tf

--- terraform/linux/main.tf
+++ terraform/linux/main.tf
@@ -9,11 +9,14 @@ resource "random_id" "tp_node_id" {
   }
 }
 
+# --- creates the keys to be able to ssh into the instances --- #
+
 resource "aws_key_pair" "tp_auth" {
   key_name   = var.key_name
   public_key = file(var.public_key_path)
 }
 
+# --- creates the instance for linux ubuntu --- #
 resource "aws_instance" "tp_node" {
   count                  = var.instance_count
   instance_type          = var.instance_type
